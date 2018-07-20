30e6e9f7b8cb3ea636b470dd352ba48f2db4b1d6
synthetiquely <anatoliybelobrovik@gmail.com>
2018-07-20 13:07:42 +0300

    Style chips

---
+++ src/components/Chip/Chip.vue
@@ -0,0 +1,47 @@
+<template>
+  <div tabindex="0" class="chip">{{text}}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.chip {
+  height: 30px;
+  margin: 5px;
+  padding: 0 15px;
+  display: flex;
+  align-items: center;
+  border-radius: 20px;
+  font-size: 13px;
+  line-height: 15px;
+  font-weight: 600;
+  color: var(--black);
+  background-color: var(--lighten-gray);
+  cursor: pointer;
+  outline-color: var(--dark-yellow);
+}
+
+.chip:first-of-type {
+  margin-left: 0;
+}
+
+..chip:last-of-type {
+  margin-right: 0;
+}
+
+.chip.active,
+.chip:hover,
+.chip:active,
+.chip:focus {
+  background-color: var(--yellow);
+}
+</style>
